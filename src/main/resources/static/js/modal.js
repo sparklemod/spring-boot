3c302d9990ca0807efc963bdcde82236999cc8ce
rina <katerina5314@mail.ru>
2025-03-21 00:56:39 +0300

    modal

--- src/main/resources/static/js/modal.js
+++ src/main/resources/static/js/modal.js
@@ -1,10 +1,3 @@
-$( '#multiple-select-field' ).select2( {
-    theme: "bootstrap-5",
-    width: $( this ).data( 'width' ) ? $( this ).data( 'width' ) : $( this ).hasClass( 'w-100' ) ? '100%' : 'style',
-    placeholder: $( this ).data( 'placeholder' ),
-    closeOnSelect: false,
-} );
-
 function openEditModal(userId) {
     document.getElementById("editModal").style.display = "flex";
 
